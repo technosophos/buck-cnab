0fa8edccffa01e6a32bd3f70b98f846260cc4c97
Matt Butcher <matt.butcher@microsoft.com>
2019-11-05 16:02:10 -0700

    updating brigade.js
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- brigade.js
+++ brigade.js
@@ -1,4 +1,4 @@
-const { events } = require("brigadier");
+const { events, Job } = require("brigadier");
 
 events.on("resource_added", handle);
 events.on("resource_modified", handle);
@@ -6,7 +6,24 @@ events.on("resource_deleted", handle);
 events.on("resource_error", handle);
 
 function handle(e, p) {
+
     console.log("buck-porter is running")
-    let obj = JSON.parse(e.payload);
+    let o = JSON.parse(e.payload);
     console.log(obj);
+
+    let cmd = "porter version";
+    switch (o.spec.action) {
+        case "install":
+        case "upgrade":
+        case "uninstall":
+            cmd = `porter ${o.spec.action} ${o.metadata.name} --tag ${o.spec.bundle}`
+            break;
+        default:
+            cmd = `porter invoke --action ${o.spec.action} ${o.metadata.name} --tag ${o.spec.bundle}`
+    }
+
+    let jorb = new Job("porter-run", "technosophos/porter:latest");
+    jorb.tasks = [cmd]
+
+    return jorb.run();
 }
